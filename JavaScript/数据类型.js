296a17f83f6bfdedc54a10989ddeebbec88f002e
long.wang <long.wang@joowing.com>
2021-05-22 01:11:13 +0800

    函数学习

--- JavaScript/数据类型.js
+++ JavaScript/数据类型.js
@@ -2,3 +2,36 @@
 // 如果不同的变量名指向同一个对象，那么它们都是这个对象的引用，也就是说指向同一个内存地址。
 // 函数与其他数据类型地位平等，所以函数又称第一等公民
 // 函数本身也是一个值，也有自己的作用域。他的作用域与变量一样，就是其声明所在的作用域，与其运行时所在的作用域无关。
+var obj = {
+  get p() {
+    return 'getter'
+  },
+  set p(value) {
+    console.log('setter', value)
+  }
+}
+// 对象的copy
+var extend = function(to, from) {
+  for (var property in from) {
+    to[property] = from[property]
+  }
+  return to
+}
+var extend = function(to, from) {
+  for (var property in from) {
+    if (from.hasOwnProperty(property)) continue;
+    Object.defineProperty(
+      to,
+      property,
+      Object.getOwnPropertyDescriptor(from. property)
+    )
+  }
+  return to
+}
+extend({}, {
+  a: 1
+})
+
+extend({}, {
+  get a() { return 1 }
+})
