dca6ca4a9454e18b35588cbd18682cebf53fc6c9
wanglong <18392700041@163.com>
2021-05-26 17:57:43 +0800

    my promise finished

--- lg-learn/part1-JavaScript/MyPromise.js
+++ lg-learn/part1-JavaScript/MyPromise.js
@@ -4,19 +4,25 @@ const REJECTED = 'reject'
 
 class MyPromise {
   constructor (executor) {
-    executor(this.resolve, this.reject)
+    try {
+      executor(this.resolve, this.reject)
+    } catch (e) { // 异常处理
+      this.reject(e)
+    }
+
   }
 
   status = PENDING
   successCallback = []
   failCallback = []
+
   resolve = (value) => {
     if (this.status !== PENDING) return
     this.value = value
     this.status = FULFILLED
     //this.successCallback && this.successCallback(this.value)
     while(!!this.successCallback.length) {
-      this.successCallback.shift()(this.value)
+      this.successCallback.shift()()
     }
   }
 
@@ -26,7 +32,7 @@ class MyPromise {
     this.status = REJECTED
     //this.failCallback && this.failCallback(this.reason)
     while(!!this.failCallback.length) { //
-      this.failCallback.shift()(this.reason)
+      this.failCallback.shift()()
     }
   }
 
@@ -36,25 +42,122 @@ class MyPromise {
    * @param {*} failCallback
    */
   then (successCallback, failCallback) {
+    // 处理没有参数的情况
+    successCallback = successCallback ? successCallback : value => value
+    failCallback = failCallback ? failCallback : reason => { throw reason }
     let promise2 = new MyPromise((resolve, reject) => {
       if (this.status === FULFILLED) {
-        let x = successCallback(this.value)
-        // 判断x的值是普通值是promise对象
-        resolvePromise(x, resolve, reject)
+        setTimeout( () => {
+          try {
+            let x = successCallback(this.value)
+            // 判断x的值是普通值是promise对象
+            // promise2需要异步处理
+            resolvePromise(promise2, x, resolve, reject)
+          } catch (e) {
+            reject(e)
+          }
+
+        }, 0)
+
       } else if (this.status === REJECTED) {
-        failCallback(this.reason)
+        setTimeout( () => {
+          try {
+            let x = failCallback(this.reason)
+            // 判断x的值是普通值是promise对象
+            // promise2需要异步处理
+            resolvePromise(promise2, x, resolve, reject)
+          } catch (e) {
+            reject(e)
+          }
+
+        }, 0)
+
       } else { // 异步执行
-        this.successCallback.push(successCallback)
-        this.failCallback.push(failCallback)
+        this.successCallback.push( () => {
+          setTimeout( () => {
+            try {
+              let x = successCallback(this.value)
+              // 判断x的值是普通值是promise对象
+              // promise2需要异步处理
+              resolvePromise(promise2, x, resolve, reject)
+            } catch (e) {
+              reject(e)
+            }
+
+          }, 0)
+        })
+        this.failCallback.push( () => {
+          setTimeout( () => {
+            try {
+              let x = failCallback(this.reason)
+              // 判断x的值是普通值是promise对象
+              // promise2需要异步处理
+              resolvePromise(promise2, x, resolve, reject)
+            } catch (e) {
+              reject(e)
+            }
+
+          }, 0)
+        })
       }
     })
 
     return promise2
 
   }
+
+  finally (callback) {
+    return this.then( value => {
+      return MyPromise.resolve(callback()).then( () => value )
+      // callback()
+      // return value
+    }, reason => {
+      return MyPromise.resolve(callback()).then( () => { throw reason } )
+      // callback()
+      // throw reason
+    })
+  }
+
+  catch (failCallback) {
+    return this.then( undefined, failCallback)
+  }
+
+  static all (array) {
+    let result = []
+    let index = 0
+
+    return new MyPromise((resolve, reject) => {
+      function addData (key, value) {
+        result[key] = value
+        index++;
+        if (index === array.length) {// 保证每一项都执行完成 然后返回
+          resolve(result)
+        }
+      }
+      for (let i = 0; i < array.length; i++) {
+        let current = array[i]
+        if (current instanceof MyPromise) {
+          current.then( value => addData(i, value), reason => reject(reason))
+        } else {
+          // 普通值
+          addData(i, current)
+        }
+      }
+    })
+  }
+
+  static resolve (value) {
+    if (value instanceof MyPromise) {
+      return value
+    }
+    return new MyPromise(resolve => resolve(value))
+  }
 }
 
-function resolvePromise (x, resolve, reject) {
+function resolvePromise (promise2, x, resolve, reject) {
+  if (promise2 == x) { // 不能自己调用自己
+    return reject(new TypeError('Chaining cycle detected for promise #<Promise>'))
+  }
   if (x instanceof MyPromise) {
     x.then(resolve, reject)
   } else {
