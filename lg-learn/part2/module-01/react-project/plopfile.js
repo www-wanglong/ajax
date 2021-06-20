92a2b1754824d469d87c2f3adce2aa65ff641131
wanglong <18392700041@163.com>
2021-06-20 23:49:35 +0800

    工程化模块提交

---
+++ lg-learn/part2/module-01/react-project/plopfile.js
@@ -0,0 +1,30 @@
+module.exports = plop => {
+  plop.setGenerator('component', {
+    description: 'create a component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'component name',
+        default: 'MyComponent'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/components/{{name}}/{{name}}.js',
+        templateFile: 'plop-templates/component.hbs'
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{name}}/{{name}}.css',
+        templateFile: 'plop-templates/component.css.hbs'
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{name}}/{{name}}.test.js',
+        templateFile: 'plop-templates/component.test.hbs'
+      }
+    ]
+  })
+}
