3bebe1770098e5d818abb581b5928061f7b64385
wanglong <18392700041@163.com>
2021-06-21 00:06:15 +0800

    node 实现脚手架提交

--- lg-learn/part2/module-01/sample-scaffolding/cli.js
+++ lg-learn/part2/module-01/sample-scaffolding/cli.js
@@ -5,8 +5,11 @@
 console.log('cli working')
 // 脚手架的工作过程
 // 1.通过命令行交互
+
+const fs = require('fs')
 const path = require('path')
 const inquirer  = require('inquirer')
+const ejs = require('ejs')
 
 inquirer.prompt([
   {
@@ -16,4 +19,24 @@ inquirer.prompt([
   }
 ]).then( answer => {
   console.log(answer)
+  // 模板目录
+  const tmpDir = path.join(__dirname, 'templates')
+  // 目标目录
+  const destDir = process.cwd()
+
+  // 读取文件
+  fs.readdir(tmpDir, (err, files) => {
+    if (err) throw err
+    files.forEach( file => {
+      console.log(file)
+      // 模板引擎渲染
+      ejs.renderFile(path.join(tmpDir, file), answer, (error, result) => {
+        if (error) throw error
+        // 将文件写到目标目录
+        fs.writeFile(path.join(destDir, file), result, () => {
+
+        })
+      })
+    })
+  })
 })
