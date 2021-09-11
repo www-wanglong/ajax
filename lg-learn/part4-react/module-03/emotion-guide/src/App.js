fe7119b4bbdd1cdcf56fc8c34e1ac0689f75a091
wanglong <18392700041@163.com>
2021-09-11 23:45:32 +0800

    css in js

---
+++ lg-learn/part4-react/module-03/emotion-guide/src/App.js
@@ -0,0 +1,12 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+
+function App() {
+
+  return (
+    <div css={{width: 200, height: 200, backgroundColor: 'red'}}></div>
+  );
+}
+
+export default App;
