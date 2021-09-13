a46a1a9f834466fe2536cd24c24a70891279915f
wanglong <18392700041@163.com>
2021-09-13 09:12:12 +0800

    css in js

--- lg-learn/part4-react/module-03/emotion-guide/src/App.js
+++ lg-learn/part4-react/module-03/emotion-guide/src/App.js
@@ -1,12 +1,107 @@
-/** @jsxRuntime classic */
-/** @jsx jsx */
-import { jsx } from '@emotion/core';
+import React from 'react';
+import styled from '@emotion/styled'
+import { css, keyframes, Global } from '@emotion/core'
+
+const styles = css`
+  body {
+    margin: 0;
+  }
+  a {
+    color: red;
+  }
+`;
+// 动画使用
+const move = keyframes`
+  0% {
+    background: skyblud;
+    left: 0;
+    top: 0;
+  }
+  100% {
+    background: tomato;
+    left: 600px;
+    top: 300px;
+  }
+`
+
+
+const box = css`
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  animation: ${move} 2s ease infinite alternate
+`;
+
+const Button = styled.button`
+  width: 100px;
+  height: 30px;
+  background: ${ props => props.bgColor || 'skyblue' }
+`
+// css选择器 &
+const Container =  styled.div`
+  width: 100px;
+  height: 1000px;
+  background: skyblue;
+  &:hover {
+    background: pink;
+  }
+  & > span {
+    color: yellow;
+  }
+`;
+
+const Container1 = styled.div({
+  width: 1000,
+  background: 'pink',
+  margin: '0 auto',
+});
+
+// 为组件添加默认的样式
+const Demo = ({ className }) => <div className={className}>Demo</div>;
+
+const Fancy = styled(Demo)`
+  color: red;
+`
+const Fancy2 = styled(Demo)({
+  background: 'tomato'
+})
+
+// const Child = styled.div`
+//   color: red;
+// `
+
+// const Parent = styled.div`
+//   ${Child} {
+//     color: green
+//   }
+// `
+
+// 通过父组件设置子组件的样式
+const Child = styled.div({
+  color: 'red'
+})
+
+const Parent = styled.div({
+  [Child]: {
+    color: 'yellow'
+  }
+})
 
 function App() {
 
-  return (
-    <div css={{width: 200, height: 200, backgroundColor: 'red'}}></div>
-  );
+  return <div css={box}></div>
+
+  return <div>
+    <Global styles={styles}></Global>
+    <Container>
+      <span>hehe</span>
+      <Button as="a" bgColor="red">button</Button>
+      <Fancy2></Fancy2>
+      <Child>child</Child>
+      <Parent><Child>Parent child</Child></Parent>
+    </Container>
+  </div>;
 }
 
 export default App;
+
--- lg-learn/part4-react/module-03/emotion-guide/src/index.js
+++ lg-learn/part4-react/module-03/emotion-guide/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ThemeProvider } from 'emotion-theming';
+
+const theme = {
+  colors: {
+    primary: 'tomato'
+  }
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}><App /></ThemeProvider>,
   document.getElementById('root')
 );
 
