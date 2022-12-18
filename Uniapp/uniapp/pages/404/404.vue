b19ca2ef98b35dca2dee93f333b9904959ea7281
王龙 <18392700041@163.com>
2022-12-18 21:53:39 +0800

    uniapp learn

---
+++ Uniapp/uniapp/pages/404/404.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		<text>页面不存在!!</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		
+		onLoad () {
+			let timer = setTimeout(() => {
+				clearTimeout(timer)
+				uni.switchTab({
+					url: '/pages/index/index'
+				})
+			}, 2000)
+		},
+		
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
